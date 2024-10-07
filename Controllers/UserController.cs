635decef6fa77e43689abdfdc22dc79f1c6b144d
gaultier.large <gaultier.large@diiage.org>
2024-10-07 09:06:17 +0200

    Ajoutez des fichiers projet.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Archi.AppUserManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Users : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
